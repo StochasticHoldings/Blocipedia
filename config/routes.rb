a3ef5184eba5202bbf03dff4ab3f10fba50ff3f4
StochasticHome <StochasticHoldings@gmail.com>
2017-11-11 17:30:35 -0500

    you can sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
-   root '/users/sign_in'
+   #root '/users/sign_in'
+   #.
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
