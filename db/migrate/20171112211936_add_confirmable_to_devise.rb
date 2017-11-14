ae44589fda0d70b15b009bdb7405f917670de5ae
StochasticHome <StochasticHoldings@gmail.com>
2017-11-14 15:09:19 -0500

    markdown-implemented

--- db/migrate/20171112211936_add_confirmable_to_devise.rb
+++ db/migrate/20171112211936_add_confirmable_to_devise.rb
@@ -1,6 +1,7 @@
 class AddConfirmableToDevise < ActiveRecord::Migration
   # Note: You can't use change, as User.update_all will fail in the down migration
   def up
+    add_column :users, :role, :integer, default: 0, null: false
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
